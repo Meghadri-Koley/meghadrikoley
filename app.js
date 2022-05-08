27444f53de21df877ce2eeaac5cfd7faf0980a55
Meghadri-Koley <meghadrikoley9@gmail.com>
2022-05-09 02:14:43 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
+var typed = new Typed(".typing",{
+    strings: ["Web Developer", "Graphics Designer", "CSE Undergrad","Data Science Enthusiast","Cloud Enthusiast"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop:true,
+});
+var typed2 = new Typed(".typing-2",{
+    strings: ["Google Cloud", "Azure", "AWS","ML|AI","Graphics Design","Cyber Security","DevOps"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop:true,
+});
+typed();
+typed2();
